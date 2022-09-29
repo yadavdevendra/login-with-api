a53430b098815da74f75ecaafa2dd6f84d1d4143
yadavdevendra <devendrayadav2022aug@cedcommerce.com>
2022-09-29 19:28:57 +0530

    login with api

--- src/components/Dashboardadd.jsx
+++ src/components/Dashboardadd.jsx
@@ -1,17 +1,13 @@
-import { Select,  Grid,  TextField, Button } from "@shopify/polaris";
+import { Select, Grid, TextField, Button } from "@shopify/polaris";
 import { useState, useCallback, useEffect } from "react";
 import { Heading } from "./Heading";
 
 function Dashboardadd() {
   const [selected, setSelected] = useState([]);
-  const [title, settitle] = useState([]);
-  const [option , setOption] = useState([])
-  const token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyX2lkIjoiMiIsInJvbGUiOiJhZG1pbiIsImV4cCI6MTY2NDQ1ODI2OCwiaXNzIjoiaHR0cHM6XC9cL2FwcHMuY2VkY29tbWVyY2UuY29tIiwiYXVkIjoiMTAzLjk3LjE4NC4xMDYiLCJ0b2tlbl9pZCI6IjQwMzgxOSJ9.LYCLrZMcgXFrc-e8TSUzwgWS0_2HzKqQRXgGOQX6Pl0WtTi783DJsimq-XH1o0sEmnFtZZIh188Bc4VNf8SICZu2zFEsbTjPU4iJppvk_KssekTbhk7xwwH0CrY8jUIdALERS217yUNl8dg21bxZn-z0pC6fPr6ZOVFRQqSyDg6qc8IGoSgth8Nf2SVhvjjPB7oB6qjhVhdJlAX1wq9Pl1w4A385-f8IRPWJ4hZjNN9otunCLu0JhDGDpe_tc8PCydE_ZGGTML05D913OT6XnYKvgfnU0OhjN3IsDUplwmcSBEvGk4dYULldgs7wg6wPvtthvEkC316ryAnCZziZLw`;
+  const token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyX2lkIjoiMiIsInJvbGUiOiJhZG1pbiIsImV4cCI6MTY2NDQ3NDI2OSwiaXNzIjoiaHR0cHM6XC9cL2FwcHMuY2VkY29tbWVyY2UuY29tIiwiYXVkIjoiMTAzLjk3LjE4NC4xMDYiLCJ0b2tlbl9pZCI6IjQwNDEzMCJ9.HvKmgPkUl4RsYfNtoVHFLPLYK9kJGOdrUX0i_PDuBelZZfnxHcHuOMmy0mWykXYiMZ3KBTAO38Oy-u4ftbBGy6ucf8SRdinQWSXG6GY-JMPUxiFXp6i9vmapXwLfrJF1IphBlqWIT3SWyrhlRF7SoifQMcF_jcH42vvkYzENchT8orGoYFZy8cv82vpwDsGHmQ1zIgkO2LmTFFc1KBcHQOiCXOABPCYOc_Fg4Uw6hafw0P5yYZjef7LGSYfEB4rUDO8li6lOxt67Xcnk3Xwu3fqKUDcxEhBwN1yrKU6Ih-HfRsSn4AY_aV5nDoI8i-eOw0zBESBhwiQbRXQiJrub3A`;
 
   const handleSelectChange = useCallback((value) => setSelected(value), []);
-   const options = [
-     { label: "Row Per Page", value: "today" }
-   ];
+  const options = [{ label: "Row Per Page", value: "today" }];
   const heading = [
     "UserId",
     "Catlog",
@@ -33,9 +29,9 @@ function Dashboardadd() {
     )
       .then((res) => res.json())
       .then((data) => {
-        setSelected(data.data.rows);
+        setSelected(data?.data?.rows);
 
-        console.log(data.data.rows);
+        console.log("row data", data);
       })
 
       .catch((error) => console.log(error));
@@ -44,7 +40,7 @@ function Dashboardadd() {
   return (
     <div>
       <div className="rowperpage">
-        <Grid >
+        <Grid>
           <Select options={options} onChange={handleSelectChange} />
           <Button>View Columns</Button>
         </Grid>
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -4,7 +4,7 @@ import { Select, Grid, Card, TextField } from "@shopify/polaris";
 export const Heading = ({item}) => {
 // const[selected , setSelected] = useState("");
    const options = [
-     { label: "Today", value: "today" },
+     { label: "Equals", value: "today" },
      { label: "Yesterday", value: "yesterday" },
      { label: "Last 7 days", value: "lastWeek" },
    ];
@@ -15,15 +15,14 @@ function handleSelectChange(){
   return (
     <div className="heading">
       <Grid>
-        <Grid.Cell columnSpan={{ xs: 3, sm: 8, md: 9, lg: 12, xl: 12 }}>
-          <Card title={item} >
+        <Grid.Cell columnSpan={{ xs: 2, sm: 12, md: 12, lg: 12, xl: 12 }}>
             <Select
+              label={item}
               options={options}
               onChange={handleSelectChange}
-            //   value={selected}
+              //   value={selected}
             />
             <TextField placeholder={item} />
-          </Card>
         </Grid.Cell>
       </Grid>
     </div>
